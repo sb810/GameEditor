9e164071aa0c98267e940ec3e703aff94a823906
sb810 <sb810@hotmail.com>
2023-10-10 00:16:01 -0400

    added gui pro kit + champselect cleanup

--- Assets/Scripts/FindPlayer.cs
+++ Assets/Scripts/FindPlayer.cs
@@ -1,17 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 using UnityEngine.UI;
 
 public class FindPlayer : MonoBehaviour
 {
     public ChampSelect champSelect;
-    public Image button;
-    public GameObject prefab;
+    public GameObject button; // button from object selector, should contain a PrefabInButton
+    [FormerlySerializedAs("button")] public Image buttonImage; // previous button<s image (can be in another gameobject)
+    public GameObject prefab; // button from ChampSelect, should contain a PrefabInButton
     void Start()
     {
         champSelect.player = gameObject;
-        button.sprite = gameObject.GetComponent<SpriteRenderer>().sprite;
+        buttonImage.sprite = gameObject.GetComponent<SpriteRenderer>().sprite;
         button.gameObject.GetComponent<PrefabInButton>().prefab = prefab.GetComponent<PrefabInButton>().prefab;
     }
 

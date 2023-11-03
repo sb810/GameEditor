73b17ff274a639933b84c3ebe1055b3811df1c98
sb810 <sb810@hotmail.com>
2023-11-02 22:53:54 -0400

    even more http debug, and potential fix

--- Assets/Scripts/PlayerData/NetworkManager.cs
+++ Assets/Scripts/PlayerData/NetworkManager.cs
@@ -16,43 +16,44 @@ namespace PlayerData
         public IEnumerator SendWebRequest(string method)
         {
             WebData dataToSave = PlayerDataManager.Data;
-            
+
             string jsonData = JsonUtility.ToJson(dataToSave);
             string uri = "https://api.studioxp.ca/items/game_editor_data_v2" + (method is "PATCH" or "GET"
-                ? "/" + dataToSave.id 
+                ? "/" + dataToSave.id
                 : "");
-            
-            Debug.Log("Preparing "+method+" webrequest. URI is " + uri + ", data is " + jsonData);
-            
+
+            Debug.Log("Preparing " + method + " webrequest. URI is " + uri + ", data is " + jsonData);
+
             using UnityWebRequest request = new UnityWebRequest(uri, method);
             request.SetRequestHeader("Content-Type", "application/json");
             request.SetRequestHeader("Authorization", "Bearer 3prCBvKlJncjeXjAOROUBQZ3qIUCMHDQ");
             byte[] rawData = Encoding.UTF8.GetBytes(jsonData);
-            request.uploadHandler = new UploadHandlerRaw(rawData);
+            if (method is not "GET") request.uploadHandler = new UploadHandlerRaw(rawData);
             request.downloadHandler = new DownloadHandlerBuffer();
             yield return request.SendWebRequest();
 
             if (request.result is not UnityWebRequest.Result.Success)
             {
-                
                 if (method is "PATCH" or "GET")
                 {
-                    Debug.Log(method + " error ! Trying again with POST...\nResponse : "+request.responseCode + "; " + request.error + "; " + request.downloadHandler.text);
+                    Debug.Log(method + " error ! Trying again with POST...\nResponse : " + request.responseCode + "; " +
+                              request.error + "; " + request.downloadHandler.text);
                     PlayerDataManager.Data.id = "";
                     UploadNewSaveData();
                     yield break;
                 }
-                
+
                 // Debug.LogError(method + " error in NetworkManager.CreateWebRequest function : " + request.downloadHandler.text);
             }
             else
             {
                 if (request.downloadHandler == null || string.IsNullOrEmpty(request.downloadHandler.text))
                 {
-                    Debug.LogError(method + " error in NetworkManager.CreateWebRequest function : " + request.downloadHandler.text);
+                    Debug.LogError(method + " error in NetworkManager.CreateWebRequest function : " +
+                                   request.downloadHandler.text);
                     yield break;
                 }
-                
+
                 if (method is "POST" or "GET")
                 {
                     // The server returns an ID, which we then use
@@ -65,7 +66,7 @@ namespace PlayerData
                         jsonData = jsonData.Remove(0, 1);
                     if (jsonData.EndsWith("]"))
                         jsonData = jsonData.Remove(jsonData.Length - 1, 1);
-                    
+
                     Debug.Log("AFTER JSON TRIMMING : " + jsonData);
                     PlayerDataManager.Data = JsonUtility.FromJson<WebData>(jsonData);
                     PlayerPrefs.SetString("clientID", PlayerDataManager.Data.id);
@@ -75,12 +76,12 @@ namespace PlayerData
                 Debug.Log(method + " complete in NetworkManager.UpdateData !\nDATA : " + PlayerDataManager.Data);
             }
         }
-        
+
         public void UploadNewSaveData()
         {
             StartCoroutine(SendWebRequest("POST"));
         }
-        
+
         public void UpdateNetworkSavedData()
         {
             StartCoroutine(SendWebRequest("PATCH"));
